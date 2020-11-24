9241a539bcbf58c41095b90cd2ee8ac84c13c276
AnonymousVip <74592146+AnonymousVip@users.noreply.github.com>
2020-11-24 09:29:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -493,6 +493,51 @@ echo $output123;
 }
 }
 }
+	if (startsWith($text,'/mean')) {
+	$word_array = explode(' ', $text);
+	$word = $word_array['1'];
+echo "https://api.urbandictionary.com/v0/define?term=$word";
+	  $ch1 = curl_init();
+curl_setopt($ch1, CURLOPT_URL, "https://api.urbandictionary.com/v0/define?term=$word"); 
+curl_setopt($ch1, CURLOPT_POST, false); 
+curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1); 
+    $output21 = curl_exec($ch1);
+$json12 = json_decode($output21,true);
+    curl_close($ch1);
+    echo $output21;
+    $list = $json12['list'];
+	$des = $json12['list']['0']['definition'];
+$damns = $json12['list']['0']['example'];
+if ($des == true) {
+$mean = "
+<b>Meaning Of Word [$word] </b> =>
+==========================
+<b>$des
+\n\n$damns</b>
+
+
+==========================
+
+<i>Extracted From => Stark Dictionary Services Pvt. Ltd.</i>";
+$message_send_meaning = [
+	'chat_id'=>$cid,
+	'text' => $mean,
+	'parse_mode'=>'HTML',
+	'reply_to_message_id'=>$mid,
+];
+
+botaction("sendMessage",$message_send_meaning);
+}
+else{
+    	$not_found = [
+    'chat_id'=>$cid,
+	'text' => '<b>Please Give Meaning full Words dude !</b>',
+	'parse_mode'=>'HTML',
+	'reply_to_message_id'=>$mid,
+];
+botaction("sendMessage",$not_found);
+}
+}
 }
 else{
 	echo "Hi";
