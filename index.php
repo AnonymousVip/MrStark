da2f47ffa991ff5ea896dd86611962c3b44627b0
AnonymousVip <74592146+AnonymousVip@users.noreply.github.com>
2020-11-26 14:48:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -538,6 +538,36 @@ else{
 botaction("sendMessage",$not_found);
 }
 }
+	if (startsWith($text,'/logo')) {
+		########LINKSIND##########
+	$font_genarate_text1 = str_replace('/logo', "", $text);
+	$font_genarate_text = str_replace(' ', "", $font_genarate_text1);
+	if ($font_genarate_text == '') {
+		echo "hell";
+		$send_error = [
+			'chat_id'=>$cid,
+			'reply_to_message_id'=>$mid,
+			'parse_mode'=>'HTML',
+			'text'=>"<b>Please Give Me Some Text For Generating Dude..</b>"
+		];
+		botaction("sendMessage",$send_error);
+	}
+	else{
+$font_list = array("https://www.linksind.net/tigerzindahai/spyder.php?name=$font_genarate_text&back=style2.jpg","https://linksind.net/arjunreddy/spyder.php?name=$font_genarate_text&back=style1.jpg","https://www.linksind.net/robo/spyder.php?name=$font_genarate_text&back=5.jpg","https://linksind.net/maari/spyder.php?name=$font_genarate_text&back=style1.jpg","https://linksind.net/cskjersey/spyder.php?name=$font_genarate_text&back=style1.jpg","https://www.linksind.net/padmavati/spyder.php?name=$font_genarate_text&back=style6.jpg");
+
+		$font = $font_list[mt_rand(0,5)];
+
+		$send_photo = [
+		'chat_id' => ''.$cid.'',
+        'caption' => '<b>Your Logo Is Generated Successfully....</b>',
+        'parse_mode' => 'HTML',
+        'reply_to_message_id'=>''.$mid.'',
+        'photo'=>"$font"
+    ];
+    botaction("sendPhoto",$send_photo);
+	}
+
+}
 }
 else{
 	echo "Hi";
