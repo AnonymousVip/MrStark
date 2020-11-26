5a2c81ee170172db773c6a47be01946808e8645e
AnonymousVip <74592146+AnonymousVip@users.noreply.github.com>
2020-11-26 19:13:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -594,68 +594,6 @@ $font_list = array("https://www.linksind.net/tigerzindahai/spyder.php?name=$font
 	}
 
 }
-	
-	if (startsWith($text,'/m')) {
-	$res = str_replace("/m", "", $text);
-	if ($res == '') {
-		$mute  = "<b>Silence Now...🤫🤫\n<a href='t.me/$reply_message_user_uname'>$reply_message_user_fname</a> Is Muted...🤐🔇</b>";
-	}
-	else{
-		$mute = "<b>Silence Now...\n<a href='t.me/$reply_message_user_uname'>$reply_message_user_fname</a> Is Muted...🤐🔇\nReason => <i>$res</i></b>";
-	}
-	if ($reply_message) {
-		if($status == 'creator' || $status == 'administrator'){
-		if (is_null($can_send_messages) or $can_send_messages == '1') {	# code
-			$muting_member = [
-			'chat_id'=>$cid,
-			'user_id'=>$reply_message_user_id,
-			'can_send_messages'=>'False'
-		];
-		botaction("restrictChatMember",$muting_member);
-		$mute_message = [
-		'chat_id'=>$cid,
-		'reply_to_message_id'=>$mid,
-		'parse_mode'=>'HTML',
-		'disable_web_page_preview'=>'True',
-		'text'=>"$mute"
-		];
-		botaction("sendMessage",$mute_message);
-}
-		else{
-			$user_is_muted = [
-			'chat_id'=>$cid,
-			'text' => "<b>There Is already a Cheese Burger 🍔 in his Mouth 😁.. \n[<i>User Is Already Muted</i>]</b>",
-			'parse_mode'=>'HTML',
-			'reply_to_message_id'=>$mid,
-				];
-			botaction("sendMessage",$user_is_muted);
-						}
-
-		}
-
-
-		else{
-		$who1 = [
-			'chat_id'=>$cid,
-			'reply_to_message_id'=>$mid,
-			'caption'=>"<b>Who The Hell Are You !! Only Admins Are Allowed To Perform This Action..\nWant A Infinity Snap ??🤜</b>",
-			'parse_mode'=>'HTML',
-			'video'=>'https://s2.gifyu.com/images/ezgif.com-gif-maker93d51c6b80ca89ad.gif',
-		];
-		botaction("sendVideo",$who1);
-		}	
-
-}
-else{
-	$no_reply = [
-			'chat_id'=>$cid,
-			'reply_to_message_id'=>$mid,
-			'parse_mode'=>'HTML',
-			'text'=>"<b>Is He A user??? Reply To A User's Message To Mute Him</b>"
-		];
-		botaction("sendMessage",$no_reply);
-}
-}
 else{
 	echo "Hi";
 	if ($typ == 'private') {
